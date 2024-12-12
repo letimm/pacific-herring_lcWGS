78ed498a0a3f7967331bbc207ab5fd3bbcd89e0b
letimm <letimm3@gmail.com>
2024-12-12 12:13:39 -0900

    Add files via upload

---
+++ scripts/CPAL-CHARFULL_concatenate_wgph_beagles.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --partition=bio
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --ntasks=24
+#SBATCH --mem=214G
+#SBATCH --job-name=CPAL-CHARFULL_concat-beagles
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=letimm@alaska.edu
+#SBATCH --output=/center1/GLASSLAB/letimm/Clupea_pallasiiFULL/job_outfiles/CPAL-CHARFULL_concatenate-wgph-beagles_%A.out
+
+ulimit -s unlimited
+ulimit -l unlimited
+
+module purge
+
+zcat /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045152.1_polymorphic-filtered.beagle.gz | head -n 1 > /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_wgph.beagle
+
+for i in /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045152.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045153.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045154.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045155.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045156.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045157.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045158.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045159.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045160.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045161.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045162.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045163.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045164.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045165.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045166.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045167.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045168.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045169.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045170.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045171.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045172.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045173.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045174.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045175.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045176.1_polymorphic-filtered.beagle.gz /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_NC-045177.1_polymorphic-filtered.beagle.gz
+do zcat $i | tail -n +2 -q >> /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_wgph.beagle
+done
+
+gzip /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_wgph.beagle
---
+++ scripts/CPAL-CHARFULL_filteredGLS.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#SBATCH --partition=bio
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --ntasks=24
+#SBATCH --mem=214G
+#SBATCH --job-name=fgls
+#SBATCH --output=/center1/GLASSLAB/letimm/Clupea_pallasiiFULL/job_outfiles/CPAL-CHARFULL_filteredGLS_%A-%a.out
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=letimm@alaska.edu
+#SBATCH --array=1-26%24
+
+ulimit -s unlimited
+ulimit -l unlimited
+
+module purge
+module load bzip2/1.0.8 GCCcore/11.3.0
+
+PATH=$PATH:/home/letimm/software/angsd
+
+JOBS_FILE=/center1/GLASSLAB/letimm/Clupea_pallasiiFULL/scripts/CPAL-CHARFULL_angsdARRAY_input.txt
+IDS=$(cat ${JOBS_FILE})
+
+for sample_line in ${IDS}
+do
+        job_index=$(echo ${sample_line} | awk -F ":" '{print $1}')
+        contig=$(echo ${sample_line} | awk -F ":" '{print $2}')
+        if [[ ${SLURM_ARRAY_TASK_ID} == ${job_index} ]]; then
+                break
+        fi
+done
+
+angsd \
+	-b /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/CPAL-CHARFULL_filtered_bamslist.txt \
+	-sites /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/paralogs/CPAL-CHARFULL_wholegenome_polymorphic_retain.sites \
+	-ref /center1/GLASSLAB/letimm/ref_genomes/GCF_900700415.2_Ch_v2.0.2_genomic.fna \
+	-r ${contig}: \
+	-out /center1/GLASSLAB/letimm/Clupea_pallasiiFULL/gls/CPAL-CHARFULL_${contig}_polymorphic-filtered \
+	-nThreads 10 \
+	-uniqueOnly 1 \
+	-remove_bads 1 \
+	-trim 0 \
+	-C 50 \
+	-minMapQ 15 \
+	-minQ 15 \
+	-doCounts 1 \
+	-setminDepth 120 \
+	-setmaxDepth 600 \
+	-doGlf 2 \
+	-GL 1 \
+	-doMaf 1 \
+	-doMajorMinor 1 \
+	-minMaf 0.05 \
+	-SNP_pval 1e-10 \
+	-doDepth 1 \
+	-dumpCounts 3 \
+	-only_proper_pairs 1
